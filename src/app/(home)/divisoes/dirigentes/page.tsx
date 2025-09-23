53439211014ea0b406792e88a172e96f368bf824
Bruno Franco <brunofrancco@gmail.com>
2025-09-23 19:28:00 +0100

    front pronto

--- src/app/(home)/divisoes/dirigentes/page.tsx
+++ src/app/(home)/divisoes/dirigentes/page.tsx
@@ -1,3 +1,41 @@
 export default function DirigentesPage() {
-  return <h1>Dirigentes</h1>
+  return (
+    <div className="bg-verde-scout/10 h-full">
+      <section className="max-w-7xl p-6  text-justify text-base/7  mx-auto text-zinc-700 sm:text-base/8">
+        <div className="relative w-full"></div>
+        <div className="pt-15 pb-6">
+          <h2 className="sm:text-base/normal text-center mb-9">
+            {/* <span className="bg-linear-to-b from-white from-70% to-azul-scout/50 to-100% pb-0.5 px-6"> */}
+            <span className="bg-azul-scout text-white pl-2 pr-4 py-2 font-bold border-l-8 border-verde-scout">
+              Dirigentes
+            </span>
+          </h2>
+        </div>
+
+        <div>
+          <p className="mb-2">
+            Os dirigentes são os associados efetivos adultos, também designado
+            de Escoteiros-Chefes e são os voluntários que orientam a atividade
+            da AEP no desempenho de um cargo nas estruturas e órgãos
+            associativos a nível nacional, regional ou local ou são formadores
+            no ativo na ENFIM.
+          </p>
+
+          <p className="mb-2">
+            Para além dos dirigentes as estruturas da AEP pode ter
+            Colaboradores. São voluntários maiores de idade que auxiliam os
+            Dirigentes em tarefas ou ações específicas no capítulo técnico,
+            pedagógico ou de gestão, sem terem os direitos e deveres daqueles.
+          </p>
+          <p className="mb-2">
+            Para a admissão e Compromisso de Honra de Dirigentes ou Colaborador
+            é condição necessária estar-se na disposição de cumprir os Estatutos
+            e Regulamentos da AEP, ter bom comportamento moral e cívico e reunir
+            as capacidades e competências adequadas, nos termos do Regulamento
+            Geral.
+          </p>
+        </div>
+      </section>
+    </div>
+  )
 }
